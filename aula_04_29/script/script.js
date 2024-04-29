9aab1ff31cf2a5b5fb5cf7f5f792a27d24bae751
Vinicius Saes <viniciusaesouza@gmail.com>
2024-04-29 10:21:20 -0300

    aula 29_04

--- aula_04_29/script/script.js
+++ aula_04_29/script/script.js
@@ -1,10 +1,39 @@
 console.log('primeira mensagem')
 
-function somar(num1 , num2) {
-    return num1 + num2
+// function somar(num1 , num2) {
+//     return num1 + num2
+// }
+
+
+// function somar(...nums) {
+
+//     let total = 0
+//     for (let num of nums){
+//         total += num
+//     }
+    
+//     return total
+// }
+
+// function aviso(){
+//     alert('Só estou passando para avisar!!!')
+// }
+
+// aviso()
+
+let recado = function(){ //Função anônima
+        alert('Criei uma função anonima')
 }
 
-console.log(somar)
+//let nome = document.getElementById('nome')
 
-console.log('segunda mensagem')
-console.log('terceira mensagem')
+let nome = document.querySelector("#nome")
+console.log(nome)
+
+nome.innerHTML = "Saes"
+
+let novo = document.getElementById("idTexto")
+
+function mudar(){
+    document.getElementById("title").innerHTML = novo.value
+}
