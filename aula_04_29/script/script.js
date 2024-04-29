433a8ca271466e2a2bbde53fa052341f895ad0a9
Vinicius Saes <viniciusaesouza@gmail.com>
2024-04-29 08:06:45 -0300

    aula 29_04

---
+++ aula_04_29/script/script.js
@@ -0,0 +1,10 @@
+console.log('primeira mensagem')
+
+function somar(num1 , num2) {
+    return num1 + num2
+}
+
+console.log(somar)
+
+console.log('segunda mensagem')
+console.log('terceira mensagem')
