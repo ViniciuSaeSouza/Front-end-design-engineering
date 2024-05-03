234288ade8428d0b1959c2a4912f8751b8ef99d3
Vinicius Saes <viniciusaesouza@gmail.com>
2024-05-03 08:36:17 -0300

    aula 05_03 DOM

---
+++ aula_05_03/script.js
@@ -0,0 +1,13 @@
+let titulo = document.querySelector(".titulo")
+console.log(titulo);
+
+
+// Retorna todas as ocorrências (Array)
+let item = document.querySelectorAll('li')[0]
+console.log(item);
+
+item.innerHTML = 'Mudei o conteúdo';
+console.log(item);
+
+let btns = document.querySelectorAll('div button')[2]
+console.log(btns);
